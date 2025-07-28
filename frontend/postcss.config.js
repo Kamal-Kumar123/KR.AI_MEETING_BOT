a6d101e8bcf2a0d427b0b259ae17cf495ae2b753
Kamal Kumar <156917034+Kamal-Kumar123@users.noreply.github.com>
2025-07-28 09:05:07 +0530

    Update postcss.config.js

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,10 +1,7 @@
-// postcss.config.js
-const tailwindcss = require('@tailwindcss/postcss');
-
 module.exports = {
   plugins: [
     require('postcss-import'),
-    tailwindcss(),  // Use as a function in Tailwind v4+
+    require('tailwindcss'),
     require('autoprefixer'),
   ],
 };
