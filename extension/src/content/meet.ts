b7f5a179b4759137eef022a6c595cd48672ff89c
Kamal Kumar <kamal.nitians@gmail.com>
2026-07-07 16:39:39 +0530

    Add extension/src/content/meet.ts

---
+++ extension/src/content/meet.ts
@@ -0,0 +1,21 @@
+import { registerPageMicHandlers } from "./mic-capture";
+
+function emitMeetingEvent(event: string, platform: string) {
+  chrome.runtime.sendMessage({ type: "MEETING_EVENT", event, platform, url: location.href });
+}
+
+const PLATFORM = "google_meet";
+
+function detectJoined() {
+  return !!document.querySelector("[data-meeting-title], [data-call-ended]");
+}
+
+registerPageMicHandlers();
+
+let wasJoined = false;
+setInterval(() => {
+  const joined = detectJoined();
+  if (joined && !wasJoined) emitMeetingEvent("meeting_started", PLATFORM);
+  if (!joined && wasJoined) emitMeetingEvent("meeting_ended", PLATFORM);
+  wasJoined = joined;
+}, 3000);
